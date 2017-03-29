36851f25da4f8c8d8ead79b19f5858daf65516d2
anadzudovic <anadzudovic1@gmail.com>
2017-03-29 10:42:46 +0200

    Implementirane metode dodajKursValuteZaDan, obrisikursValuteZaDan i vratiKjursValuteZaDan u okviru master grane

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -13,21 +13,40 @@ public class Menjacnica implements SpecifikacijaInterfejs {
 	@Override
 	public void dodajKursValuteZaDan(GregorianCalendar dan, Valuta valuta, double prodajniKurs, double kupovniKurs,
 			double SrednjiKurs) {
-		// TODO Auto-generated method stub
+		Kurs novi = new Kurs();
+		novi.setDatum(dan);
+		novi.setValuta(valuta);
+		novi.setKupovniKurs(kupovniKurs);
+		novi.setProdajniKurs(prodajniKurs);
+		novi.setSrednjiKurs(SrednjiKurs);
+		if(lista.contains(novi)!=true) lista.addLast(novi);
 		
 	}
 
 	@Override
 	public void obrisiKursValuteZaDan(GregorianCalendar dan, Valuta valuta) {
-		// TODO Auto-generated method stub
+		for(int i =0;i<lista.size();i++){
+			Kurs trenutni = new Kurs ();
+			if(trenutni.getDatum()==dan && trenutni.getValuta().equals(valuta)){
+				lista.remove(trenutni);
+				break;
+			}
+			
+			
+		}
+		
 		
 	}
 
 	@Override
 	public Kurs vratiKursValuteZaDan(GregorianCalendar dan, Valuta valuta) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		for(int i =0;i<lista.size();i++){
+			Kurs trenutni = new Kurs ();
+			if(trenutni.getDatum()==dan && trenutni.getValuta().equals(valuta)){
+				return trenutni;
+		   }
 	
-
+	      }
+	            return null;
+	}
 }
