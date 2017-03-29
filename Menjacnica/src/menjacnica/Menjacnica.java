5cf96fe49a0392ef65796842827641e193870b1e
anadzudovic <anadzudovic1@gmail.com>
2017-03-29 10:32:21 +0200

    Implementirane metode dodajKursValuteZaDan, obrisiKursValuteZaDan i vratiKursValuteZaDan u klasi Menjacnica u okviru grane implementacijainterfejsa.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -13,20 +13,34 @@ public class Menjacnica implements SpecifikacijaInterfejs {
 	@Override
 	public void dodajKursValuteZaDan(GregorianCalendar dan, Valuta valuta, double prodajniKurs, double kupovniKurs,
 			double SrednjiKurs) {
-		// TODO Auto-generated method stub
-		
+		Kurs novi = new Kurs();
+		novi.setDatum(dan);
+		novi.setKupovniKurs(kupovniKurs);
+		novi.setProdajniKurs(prodajniKurs);
+		novi.setSrednjiKurs(SrednjiKurs);
+		novi.setValuta(valuta);
+		lista.add(novi);
 	}
 
 	@Override
 	public void obrisiKursValuteZaDan(GregorianCalendar dan, Valuta valuta) {
-		// TODO Auto-generated method stub
+		for (int i =0;i<lista.size();i++){
+			if(lista.get(i).getDatum()==dan && lista.get(i).getValuta().equals(valuta)){
+				lista.remove(i);
+				return;
+			}
+		}
 		
 	}
 
 	@Override
 	public Kurs vratiKursValuteZaDan(GregorianCalendar dan, Valuta valuta) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i =0;i<lista.size();i++){
+			if(lista.get(i).getDatum()==dan && lista.get(i).getValuta().equals(valuta)){
+				return lista.get(i);
+			}
+			}
+		throw new RuntimeException("Nema kursa za taj dan i tu valutu u listi");
 	}
 	
 
