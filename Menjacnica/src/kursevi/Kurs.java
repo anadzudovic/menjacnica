268162b3ec230b3abd8cba05504199af88834767
anadzudovic <anadzudovic1@gmail.com>
2017-03-29 10:00:02 +0200

    Dodate metode equals, hashCode i toString u klase Kurs i Valuta.

--- Menjacnica/src/kursevi/Kurs.java
+++ Menjacnica/src/kursevi/Kurs.java
@@ -3,8 +3,7 @@ package kursevi;
 import java.util.GregorianCalendar;
 
 import kursevi.Valuta;
-
-public class Kurs {
+    public class Kurs {
 	private GregorianCalendar datum;
 	private Valuta valuta;
 	private double prodajniKurs;
@@ -40,6 +39,39 @@ public class Kurs {
 	public void setSrednjiKurs(double srednjiKurs) {
 		this.srednjiKurs = srednjiKurs;
 	}
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Kurs))
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null) 
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kurs [datum=" + datum + ", valuta=" + valuta + ", prodajniKurs=" + prodajniKurs + ", kupovniKurs="
+				+ kupovniKurs + ", srednjiKurs=" + srednjiKurs + "]";
+	}
 
 }
--- Menjacnica/src/kursevi/Valuta.java
+++ Menjacnica/src/kursevi/Valuta.java
@@ -1,5 +1,7 @@
 package kursevi;
 
+import kursevi.Valuta;
+
 public class Valuta {
 	private String naziv;
 	private String oznaka;
@@ -15,6 +17,38 @@ public class Valuta {
 	public void setOznaka(String oznaka) {
 		this.oznaka = oznaka;
 	}
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((oznaka == null) ? 0 : oznaka.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Valuta))
+			return false;
+		Valuta other = (Valuta) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (oznaka == null) {
+			if (other.oznaka != null)
+				return false;
+		} else if (!oznaka.equals(other.oznaka))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", oznaka=" + oznaka + "]";
+	}
 
 }
